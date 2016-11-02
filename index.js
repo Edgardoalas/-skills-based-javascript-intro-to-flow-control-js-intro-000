1ed61a35d86d8c7a8357ac78a740656deff3bf0e
edgardo <edgardo.alas.simontech@gmail.com>
2016-11-02 16:12:11 +0000

    yeaaaa

---
+++ index.js
@@ -0,0 +1,41 @@
+// write your code below!
+
+function basicTeenager(age) {
+  if(age >= 13 && age <19) {
+return "You are a teenager!"
+  } else {
+
+  }
+
+}
+
+function teenager(age) {
+  if(age >=13 && age <19) {
+    return "You are a teenager!"
+  } else {
+
+  }
+
+}
+
+function teenager(age) {
+  if(age >=13 && age <19) {
+    return "You are not a teenager!"
+  } else {
+
+  }
+
+}
+
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
